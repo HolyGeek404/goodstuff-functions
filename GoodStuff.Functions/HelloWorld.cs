ac0ade626032c47be0e56afa5ee6da71aec4ffe0
HolyGeek404 <wmatkowski404@proton.me>
2025-11-07 12:28:18 +0100

    Works locally

--- GoodStuff.Functions/HelloWorld.cs
+++ GoodStuff.Functions/HelloWorld.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using Azure.Core;
+using Azure.Identity;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 
@@ -9,6 +11,12 @@ public class HelloWorld
     [Function("HelloWorld")]
     public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
     {
+        var credential = new DefaultAzureCredential();
+        var token = await credential.GetTokenAsync(
+            new TokenRequestContext(["api://56b1c593-a584-4622-b223-bcf0fb117cb1/.default"])
+        );
+
+        
         var response = req.CreateResponse(HttpStatusCode.OK);
         await response.WriteStringAsync("Hello world");
         return response;
